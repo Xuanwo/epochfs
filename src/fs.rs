d5da5620f5f56b146a31e729f829628acc193988
Xuanwo <github@xuanwo.io>
2025-02-07 17:24:59 +0800

    feat: Implement chunk id calculation and write (#8)
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- src/fs.rs
+++ src/fs.rs
@@ -1,12 +1,11 @@
 use anyhow::Result;
 use opendal::Operator;
-use prost::Message;
 use sqlx::SqlitePool;
 
 /// Fs is the main entry point for the epoch filesystem.
 #[derive(Debug, Clone)]
 pub struct Fs {
-    db: SqlitePool,
+    _db: SqlitePool,
     _op: Operator,
 }
 
@@ -37,25 +36,6 @@ impl Fs {
         .execute(&db)
         .await?;
 
-        Ok(Fs { db, _op: op })
-    }
-
-    /// Add a file to the filesystem.
-    ///
-    /// This function will add file record in the database. The chunk must
-    /// have already been uploaded to the storage.
-    pub async fn commit_file_in_db(&self, path: String, chunk_ids: Vec<String>) -> Result<()> {
-        let chunk_ids = super::specs::FileChunkIds { ids: chunk_ids };
-        let chunk_id_content = chunk_ids.encode_to_vec();
-
-        sqlx::query!(
-            r#"INSERT INTO files (path, chunks) VALUES (?, ?)"#,
-            path,
-            chunk_id_content
-        )
-        .execute(&self.db)
-        .await?;
-
-        Ok(())
+        Ok(Fs { _db: db, _op: op })
     }
 }
